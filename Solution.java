c29746fd942c66a802d26588190f40fac5004327
OctaPinball <scholtz.balint@gmail.com>
2022-12-12 22:31:33 +0100

    BestSeparation completed

--- Solution.java
+++ Solution.java
@@ -9,8 +9,76 @@ public class Solution{
         return -((pCat1) * (Math.log(pCat1) / Math.log(2))) - ((pCat2) * (Math.log(pCat2) / Math.log(2)));
     }
 
+    /*
+    public static double infoGain(int[] left, int leftSize, int[] right, int rightSize, int parentSize){
+        double lWeight = (double) leftSize / parentSize;
+        double rWeight = (double) rightSize / parentSize;
+
+        int lTrue = 0;
+        int lFalse = 0;
+        for(int i = 0; i < leftSize; i++)
+        {
+            if()
+        }
+    }
+*/
     public static int[] getBestSeparation(int[][] features, boolean[] labels){
         int[] answer = {0,0};
+        double bestEntropy = -1;
+        for(int i = 0; i < features.length; i++)
+        {
+            for(int j = 0; j < features[i].length; j++)
+            {
+                int left[] = new int[features.length];
+                int leftSize = 0;
+                int leftTrue = 0;
+                int leftFalse = 0;
+
+                int right[] = new int[features.length];
+                int rightSize = 0;
+                int rightTrue = 0;
+                int rightFalse = 0;
+
+                for(int k = 0; k < features.length; k++)
+                {
+                    //left
+                    if(features[k][j] < features[i][j])
+                    {
+                        left[leftSize] = features[k][j];
+                        leftSize++;
+                        if(labels[k])
+                        {
+                            leftTrue++;
+                        }
+                        else
+                        {
+                            leftFalse++;
+                        }
+                    }
+
+                    //right
+                    if(features[k][j] >= features[i][j])
+                    {
+                        right[leftSize] = features[k][j];
+                        rightSize++;
+                        if(labels[k])
+                        {
+                            rightTrue++;
+                        }
+                        else
+                        {
+                            rightFalse++;
+                        }
+                    }
+                    double entropy = getEntropy(leftTrue, leftFalse) * ((double)leftSize / (leftSize + rightSize)) + getEntropy(rightTrue, rightFalse) * ((double)rightSize / (rightSize + leftSize));
+                    if(entropy < bestEntropy || bestEntropy == -1)
+                    {
+                        bestEntropy = entropy;
+                        answer = new int[]{j, features[i][j]};
+                    }
+                }
+            }
+        }
 
         return answer;
     }
